c475da9189fce0575a5b9c4102c8290debd75dcd
MD Razikul Islam Joni <mdrazikulislamjoni@gmail.com>
2023-06-11 09:16:51 +0600

    add cors and parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+
+// cors
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
     res.json({
